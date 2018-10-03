f70f2e86ca6621c2e26b1f0fe0c3d78177ce4447
jeanc72 <43798297+jeanc72@users.noreply.github.com>
2018-10-03 20:32:21 -0300

    commit inicial
    Creación clase HolaMundo, donde mostramos por pantalla el mensaje Hola Mundo !!!

---
+++ ClubHipico_Jcvargas/src/cl/clubhipico/clubhipico_jcvargas/HolaMundo.java
@@ -0,0 +1,14 @@
+package cl.clubhipico.clubhipico_jcvargas;
+
+public class HolaMundo {
+	
+	/* Comentario en
+	 *  varias
+	 *  lineas
+	 */
+	public static void main (String[] args) {
+	   // Comentario solo una linea	
+	   System.out.println("Hola Mundo !!!");
+	}
+
+}
